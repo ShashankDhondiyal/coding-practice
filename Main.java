c47f2eb29eb2b787e7bf803cf6cdf49297cc2cab
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 12:40:33 -0500

    feat: bunnyEars
    add bunnyEars method to Solutions.java
    update test cases in Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Solutions sol = new Solutions();
 
-        System.out.println(sol.factorial(1));
-        System.out.println(sol.factorial(2));
-        System.out.println(sol.factorial(3));
+        System.out.println(sol.bunnyEars(0));
+        System.out.println(sol.bunnyEars(1));
+        System.out.println(sol.bunnyEars(2));
     }
 }
--- Solutions.java
+++ Solutions.java
@@ -107,4 +107,15 @@ public class Solutions {
         return n * factorial(n - 1);
     }
 
+    /*
+     * We have a number of bunnies and each bunny has two big floppy ears. 
+     * We want to compute the total number of ears across all the bunnies recursively.
+     */
+    public int bunnyEars(int bunnies) {
+        if (bunnies == 0) {
+            return 0;
+        }
+        return 2 + bunnyEars(bunnies - 1);
+    }
+
 }
