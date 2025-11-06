968872ea28ad54d5289945628d7f79b1e2e84277
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 15:56:09 -0500

    feat(leetcode): validParentheses
    add question prompt to LeetCode.md
    add solution to LeetCode.java
    add test cases to Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
         CodingBat cb = new CodingBat();
         LeetCode lc = new LeetCode();
+
+        System.out.println(lc.isValid("()"));
+        System.out.println(lc.isValid("()[]{}"));   
+        System.out.println(lc.isValid("(]"));
+        System.out.println(lc.isValid("([])"));
+        System.out.println(lc.isValid("([)]"));
+        System.out.println(lc.isValid("["));
+        System.out.println(lc.isValid("(("));
+        System.out.println(lc.isValid("){"));
+
     }
 }
--- leetcode/src/LeetCode.java
+++ leetcode/src/LeetCode.java
@@ -1,3 +1,39 @@
+import java.util.Stack;
+
 public class LeetCode {
     
+    /* Valid Parentheses */
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+
+        Stack<Character> myStack = new Stack<>();
+
+        char[] inputs = s.toCharArray();
+
+        for (char input : inputs) {
+            switch (input){
+                case '(' -> myStack.push(input);
+                case '[' -> myStack.push(input);
+                case '{' -> myStack.push(input);
+                case ')' -> {
+                    if ((myStack.isEmpty()) || (myStack.pop() != '(')) {
+                        return false;
+                    }
+                }
+                case ']' -> {
+                    if ((myStack.isEmpty()) || (myStack.pop() != '[')) {
+                        return false;
+                    }
+                }
+                case '}' -> {
+                    if ((myStack.isEmpty()) || (myStack.pop() != '{')) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return myStack.isEmpty();
+    }
 }
