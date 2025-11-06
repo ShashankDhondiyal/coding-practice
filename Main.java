072da9d149d24238ea1cc8753125ebdcc9486504
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 12:02:04 -0500

    feat: wordsFront
    add wordsFront method to Solution.java
    update test cases in Main.java
    add import Arrays in Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solutions sol = new Solutions();
 
-        String[] test = {"a", "bb", "b", "ccc"};
-        System.out.println(sol.wordsCount(test, 1));
-        System.out.println(sol.wordsCount(test, 3));
-        System.out.println(sol.wordsCount(test, 4));
+        String[] test = {"a", "b", "c", "d"};
+
+        System.out.println(Arrays.toString(sol.wordsFront(test, 1)));
+        System.out.println(Arrays.toString(sol.wordsFront(test, 2)));
+        System.out.println(Arrays.toString(sol.wordsFront(test, 3)));
     }
 }
--- Solutions.java
+++ Solutions.java
@@ -61,4 +61,15 @@ public class Solutions {
         }
         return count;
     }
+
+    /*
+     * Given an array of strings, return a new array containing the first N strings. 
+     * N will be in the range 1..length
+     */
+    public String[] wordsFront(String[] words, int n) {
+        String[] ans = new String[n];
+        System.arraycopy(words, 0, ans, 0, n);
+        return ans;
+    }
+    
 }
