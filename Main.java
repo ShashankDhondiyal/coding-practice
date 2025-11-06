2726e78ca4dfdd028858e95ea875c28fbe5c216e
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 11:47:34 -0500

    feat: wordsCount
    add wordsCount method to Solutions.java
    update test cases in Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Solutions sol = new Solutions();
 
-        int[][] tests = {
-            {2,2,4,4},
-            {4,4,4,2,2,2},
-            {3,4,5,1,2,3},
-            {1,2},
-        };
-        
-        for (int[] test : tests) {
-            System.out.println(sol.scoresAverage(test));
-        }
+        String[] test = {"a", "bb", "b", "ccc"};
+        System.out.println(sol.wordsCount(test, 1));
+        System.out.println(sol.wordsCount(test, 3));
+        System.out.println(sol.wordsCount(test, 4));
     }
 }
--- Solutions.java
+++ Solutions.java
@@ -48,4 +48,17 @@ public class Solutions {
 
         return firstAverage > secondAverage ? firstAverage : secondAverage;
     }
+
+    /*
+     * Given an array of strings, return the count of the number of strings with the given length.
+     */
+    public int wordsCount(String[] words, int len) {
+        int count = 0;
+        for (String word : words) {
+            if (word.length() == len) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
