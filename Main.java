3936138e5956d55dc6e906bdf591efa5b449a9a7
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 11:20:41 -0500

    feat: scoresIncreasing
    add scoresIncreasing method to Solutions.java
    add testcases to Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Solutions sol = new Solutions();
 
+        int[][] tests = {
+            {1,2},
+            {1,2,3,4,5},
+            {5,4,3,2,1},
+        };
+        
+        for (int[] test : tests) {
+            System.out.println(sol.scoresIncreasing(test));
+        }
     }
 }
--- Solutions.java
+++ Solutions.java
@@ -1,3 +1,15 @@
 public class Solutions {
 
+    /*
+    * Given an array of scores, return true if each score is equal or greater than the one before.
+    * The array will be length 2 or more.
+    */
+    public boolean scoresIncreasing(int[] scores) {
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] < scores[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
