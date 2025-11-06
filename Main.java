8e7e703d3af76832750b03049ecd72e6328f6bd6
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 12:35:40 -0500

    feat: factorial
    add factorial method to Solutions.java
    update test cases in Main.java

--- Main.java
+++ Main.java
@@ -2,22 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Solutions sol = new Solutions();
 
-        int[] result = sol.copyEndy(new int[] {9, 11, 90, 22, 6}, 2);
-        for (int e : result) {
-            System.out.print(e + " ");
-        }
-        System.out.println();
-
-        result = sol.copyEndy(new int[] {9, 11, 90, 22, 6}, 3);
-        for (int e : result) {
-            System.out.print(e + " ");
-        }
-        System.out.println();
-
-        result = sol.copyEndy(new int[] {12, 1, 1, 13, 0, 20}, 2);
-        for (int e : result) {
-            System.out.print(e + " ");
-        }
-        System.out.println();
+        System.out.println(sol.factorial(1));
+        System.out.println(sol.factorial(2));
+        System.out.println(sol.factorial(3));
     }
 }
--- Solutions.java
+++ Solutions.java
@@ -92,5 +92,19 @@ public class Solutions {
         }
         return ans;
     }
-    
+
+    /*
+     * Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. 
+     * Compute the result recursively
+     */
+    public int factorial(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
 }
