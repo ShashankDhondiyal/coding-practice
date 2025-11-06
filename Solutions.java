06e2306d88232fe2c3f524e273cec7ae5a6bc88e
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-06 12:09:53 -0500

    fix: wordsFront
    update wordsFront method in Solutions.java

--- Solutions.java
+++ Solutions.java
@@ -68,7 +68,9 @@ public class Solutions {
      */
     public String[] wordsFront(String[] words, int n) {
         String[] ans = new String[n];
-        System.arraycopy(words, 0, ans, 0, n);
+        for (int i = 0; i < n; i++) {
+            ans[i] = words[i];
+        }
         return ans;
     }
     
