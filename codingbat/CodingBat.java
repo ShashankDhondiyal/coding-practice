d42905867cc18a26470b6166bd4f207b0d877419
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-11-13 21:53:31 -0500

    feat: triangle
    add triangle method to CodingBat.java

--- codingbat/CodingBat.java
+++ codingbat/CodingBat.java
@@ -186,4 +186,12 @@ public class CodingBat {
         }
         return steps;
     }
+
+    /* We have triangle made of blocks.
+     * The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. 
+     * Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows. 
+     */
+    public int triangle(int rows) {
+        return rows == 0 ? 0 : rows-- + triangle(rows);
+    }
 }
